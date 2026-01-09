cc39c014acdbfcb7f041dc6aa99136459b676ef7
sadikbaba <4821saddikhady@gmail.com>
2026-01-09 11:54:34 +0100

    firts bug

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -23,6 +23,8 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
+    //@audit missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNewPassword();
